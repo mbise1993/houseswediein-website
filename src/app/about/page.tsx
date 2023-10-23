257d52ba7cf6b78fe5803e1cf1e573eeebd194ff
Matt Bise <mbise1993@gmail.com>
2023-10-23 00:59:41 -0500

    Additional assets

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -2,6 +2,25 @@ import { Metadata } from 'next';
 
 import { SectionHeading } from '@/components/section-heading';
 
+const ASSETS = [
+  {
+    name: 'Logo (black)',
+    url: '/assets/logo-black.png',
+  },
+  {
+    name: 'Logo (white)',
+    url: '/assets/logo-white.png',
+  },
+  {
+    name: 'Logo alt (black)',
+    url: '/assets/logo-alt-black.png',
+  },
+  {
+    name: 'Logo alt (white)',
+    url: '/assets/logo-alt-white.png',
+  },
+];
+
 export const metadata: Metadata = {
   title: 'About | HWDI',
   description: 'About the band',
@@ -17,17 +36,15 @@ export default function Page() {
       </p>
 
       <SectionHeading className="mt-6">ASSETS</SectionHeading>
-      <ul>
-        <li>
-          <a target="_blank" href="/assets/logo-black.png">
-            Logo (black)
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="/assets/logo-white.png">
-            Logo (white)
-          </a>
-        </li>
+      <ul className="flex flex-col gap-4">
+        {ASSETS.map((asset) => (
+          <li key={asset.url}>
+            <img className="h-[60px]" src={asset.url} alt={asset.name} />
+            <a target="_blank" href={asset.url}>
+              {asset.name}
+            </a>
+          </li>
+        ))}
       </ul>
     </div>
   );
