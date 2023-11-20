6082eae1a42d15cc31789a0e43a93c7a7b76c8d3
Matt Bise <mbise1993@gmail.com>
2023-11-20 02:11:10 -0600

    Update flyer

--- src/app/live/shows.ts
+++ src/app/live/shows.ts
@@ -13,8 +13,7 @@ export const UPCOMING_SHOWS: Show[] = [
       'The High Cost of Playing God and The Vinous w/ Votive & Houses We Die In',
     date: 'Thurs, Nov 30th',
     flyerUrl: '/flyers/2023-11-30.jpg',
-    venueName: 'Inner Chamber',
-    venueLink: 'https://www.instagram.com/innerchamberatx/',
+    venueName: 'Linton House',
   },
 ];
 
